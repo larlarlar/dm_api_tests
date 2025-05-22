41a9e97c3ca271f4f33cc61790a1a7c8e94146aa
larlarlar <larisa.gurina.s@gmail.com>
2025-05-22 23:44:18 +0200

    test_post_v1_account_token commit

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -12,8 +12,8 @@ def test_put_v1_account_token():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'AnnH'
-    password = 'Janes'
+    login = 'AnnHata'
+    password = 'Janeserwe'
     email = f'{login}@mail.ru'
     json_data = {
         'login': login,
