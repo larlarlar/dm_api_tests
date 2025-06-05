10290bde2a203e41f9e330f68982b5d7206e7a2b
larlarlar <larisa.gurina.s@gmail.com>
2025-06-05 23:45:09 +0200

    added post_v1_account fixture

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,5 +1,7 @@
 from json import loads
 
+import pytest
+
 from helpers.account_helper import AccountHelper
 from restclient.configuration import Configuration as MailhogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
@@ -15,19 +17,34 @@ structlog.configure(
             ensure_ascii=True,
             # sort_keys=True
         )
-    ]
+    ],
 )
 
-def test_post_v1_account():
+
+@pytest.fixture
+def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = MailHogApi(configuration=mailhog_configuration)
+    return mailhog_client
+
+
+@pytest.fixture
+def account_api():
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
-
     account = DMApiAccount(configuration=dm_api_configuration)
-    mailhog = MailHogApi(configuration=mailhog_configuration)
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+    return account
 
-    login = 'tsetagain4126'
+
+@pytest.fixture
+def account_helper(account_api, mailhog_api):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
+
+
+def test_post_v1_account(account_helper):
+    login = 'newtser12345'
     password = 'KasaradysiuKasaradysiu'
     email = f'{login}@mail.ru'
 
     account_helper.register_new_user(login=login, password=password, email=email)
+    account_helper.user_login(login=login, password=password)
